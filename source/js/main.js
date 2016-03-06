c260b91453e305ae4208a7ba2999465849505e07
Leon <leonjiang1121@gmail.com>
2016-03-06 16:43:13 +0800

    finish comment blokc

--- source/js/main.js
+++ source/js/main.js
@@ -5,3 +5,14 @@ $(document).ready(function() {
 });
 
 hljs.configure({useBR: true});
+
+var duoshuoQuery = {short_name:"dogod"};
+
+(function() {
+		var ds = document.createElement('script');
+		ds.type = 'text/javascript';ds.async = true;
+		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+		ds.charset = 'UTF-8';
+		(document.getElementsByTagName('head')[0] 
+		 || document.getElementsByTagName('body')[0]).appendChild(ds);
+})();
