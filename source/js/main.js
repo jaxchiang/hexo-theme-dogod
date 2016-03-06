0015213129b3331180c6242e02e22fc5a6ed4693
Leon <leonjiang1121@gmail.com>
2016-03-06 21:57:31 +0800

    update some fuction

--- source/js/main.js
+++ source/js/main.js
@@ -2,17 +2,36 @@ $(document).ready(function() {
   $('.highlight').each(function(i, block) {
     hljs.highlightBlock(block);
   });
+   $('.pinBox').stickMe();
 });
 
+$(function() { 
+
+function scroll_fn(){
+
+    document_height = $(".content .section").height();
+    scroll_so_far = $(".content").scrollTop();
+    window_height = $(".content").height();
+    
+	max_scroll = document_height-window_height;
+
+	scroll_percentage = scroll_so_far/(max_scroll/100);
+    
+    $('.process').width(scroll_percentage + '%');
+
+}
+
+
+
+$(".content").scroll(function() {
+scroll_fn();
+});
+
+$(window).resize(function() {
+scroll_fn();
+});
+
+});
+
+
 hljs.configure({useBR: true});
-
-var duoshuoQuery = {short_name:"dogod"};
-
-(function() {
-		var ds = document.createElement('script');
-		ds.type = 'text/javascript';ds.async = true;
-		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
-		ds.charset = 'UTF-8';
-		(document.getElementsByTagName('head')[0] 
-		 || document.getElementsByTagName('body')[0]).appendChild(ds);
-})();
