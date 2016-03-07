723502b38e835f9e47d3eafed1ec14ff8e4dd0e7
Leon <leonjiang1121@gmail.com>
2016-03-07 16:33:01 +0800

    update loading function

--- source/js/main.js
+++ source/js/main.js
@@ -1,37 +1,31 @@
 $(document).ready(function() {
-  $('.highlight').each(function(i, block) {
-    hljs.highlightBlock(block);
-  });
-   $('.pinBox').stickMe();
+    $('.highlight').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
+    $('.pinBox').stickMe();
 });
 
-$(function() { 
+$(function() {
+    function scroll_fn() {
+        document_height = $(".content .section").height();
+        scroll_so_far = $(".content").scrollTop();
+        window_height = $(".content").height();
+        max_scroll = document_height - window_height;
+        scroll_percentage = scroll_so_far / (max_scroll / 100);
+        $('.process').width(scroll_percentage + '%');
+    };
 
-function scroll_fn(){
+    $(".content").scroll(function() {
+        scroll_fn();
+    });
 
-    document_height = $(".content .section").height();
-    scroll_so_far = $(".content").scrollTop();
-    window_height = $(".content").height();
-    
-	max_scroll = document_height-window_height;
-
-	scroll_percentage = scroll_so_far/(max_scroll/100);
-    
-    $('.process').width(scroll_percentage + '%');
-
-}
-
-
-
-$(".content").scroll(function() {
-scroll_fn();
+    $(window).resize(function() {
+        scroll_fn();
+    });
 });
 
-$(window).resize(function() {
-scroll_fn();
-});
+hljs.configure({ useBR: true });
 
-});
-
-
-hljs.configure({useBR: true});
+Pace.on("hide",function(){
+	$(".maindiv").removeClass('hide');
+})
